9d824e16d74362144a7c419c610f3d008433b814
Shahriar Rumel <kenkanekikunsr@gmail.com>
2022-03-18 03:59:50 +0600

    Classroom Screen Done

---
+++ src/main/java/com/example/cseduportalcse2112/Classroom.java
@@ -0,0 +1,20 @@
+package com.example.cseduportalcse2112;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Classroom extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(CSEDUPORTAL.class.getResource("ClassroomScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 680);
+        stage.setTitle("CSEDU PORTAL");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
