9bc9b917e2dc1438ecf64f5440f1013777bd8910
Shahriar Rumel <kenkanekikunsr@gmail.com>
2022-03-20 02:33:33 +0600

    Database Created and Sign Up Implemented

--- src/main/java/com/example/cseduportalcse2112/CSEDUPORTALUtils.java
+++ src/main/java/com/example/cseduportalcse2112/CSEDUPORTALUtils.java
@@ -8,10 +8,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class CSEDUPORTALUtils {
 
@@ -64,5 +66,66 @@ public class CSEDUPORTALUtils {
         stage.setTitle(Title);
         stage.setScene(new Scene(root,1100,680));
         stage.show();
+    }
+
+    public static void signUpUser(ActionEvent event,String Name,String Email,String Password,String semester,String year,String roll,String registration){
+        Connection connection = null;
+        PreparedStatement userInsert = null;
+        PreparedStatement checkUserExist = null;
+        ResultSet resultSet = null;
+
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/cseduportal","root","user");
+            checkUserExist = connection.prepareStatement("SELECT * FROM student WHERE Registration = ?");
+            checkUserExist.setString(1, registration);
+
+            resultSet = checkUserExist.executeQuery();
+
+            if(resultSet.isBeforeFirst()){
+                System.out.println("User Already Exist");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("User Already Exist");
+                alert.show();
+            }
+            else{
+                userInsert = connection.prepareStatement("INSERT INTO student (Name,Email,Password,Year,Semester,Roll,Registration) VALUES (?,?,?,?,?,?,? )");
+                userInsert.setString(1,Name);
+                userInsert.setString(2,Email);
+                userInsert.setString(3,Password);
+                userInsert.setString(4, String.valueOf(year));
+                userInsert.setString(5, String.valueOf(semester));
+                userInsert.setString(6, String.valueOf(roll));
+                userInsert.setString(7,registration);
+                userInsert.executeUpdate();
+                changeScence(event,"DashboardScreen.fxml","Dashboard",null);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(resultSet != null){
+                try{
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(userInsert != null){
+                try{
+                    userInsert.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }if(checkUserExist != null){
+                try{
+                    checkUserExist.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+
     }
 }
--- src/main/java/com/example/cseduportalcse2112/SignupScreenController.java
+++ src/main/java/com/example/cseduportalcse2112/SignupScreenController.java
@@ -5,6 +5,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,12 +14,44 @@ import java.util.ResourceBundle;
 public class SignupScreenController  implements Initializable {
     @FXML
     private Button SignupScreenLogin;
+    @FXML
+    private ChoiceBox signupSemesterChoicebox;
+    @FXML
+    private ChoiceBox signupYearChoicebox;
+    @FXML
+    private TextField studentName;
+    @FXML
+    private TextField studentEmail;
+    @FXML
+    private TextField studentPassword;
+    @FXML
+    private TextField studentRoll;
+    @FXML
+    private TextField studentReg;
+
+
+    @FXML
+    private Button signupbutton;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        signupSemesterChoicebox.getItems().addAll("Semester One","Semester Two");
+        signupSemesterChoicebox.setValue("Semester One");
+
+        signupYearChoicebox.getItems().addAll("Year One","Year Two","Year Three","Year Four");
+        signupYearChoicebox.setValue("Year Two");
+
         SignupScreenLogin.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 CSEDUPORTALUtils.changeScence(actionEvent,"LoginScreen.fxml","Log in",null);
+            }
+        });
+
+        signupbutton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                CSEDUPORTALUtils.signUpUser(actionEvent,studentName.getText(),studentEmail.getText(),studentPassword.getText(), (String) signupSemesterChoicebox.getValue(), (String) signupYearChoicebox.getValue(),studentRoll.getText(),studentReg.getText());
             }
         });
     }
